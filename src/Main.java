67e311a9a77b28fe0281e2ede3abab206816de45
Derrick Poon <zhinkee@gmail.com>
2023-03-16 20:04:32 -0500

    linked list sorting(no index).

--- src/Main.java
+++ src/Main.java
@@ -24,22 +24,4 @@ public class Main extends LinkedList{
         }
 
     }
-
-    static int nameCodeCreator(String n){
-        int code;
-        if(n.length() == 3){                                                                                            //for names that are 3 letters long-
-            code = ((n.charAt(0) - 96) * 456976) + ((n.charAt(1) - 96) * 17576) + ((n.charAt(2) - 96) * 676);           //char - 96, not 97('a') due to names-
-            return code;                                                                                                //that matches other names except for the last letter being 'a', aka karl vs karla
-        }
-        else if(n.length() == 4){                                                                                       //for names that are 4 letters long
-            code = ((n.charAt(0) - 96) * 456976) + ((n.charAt(1) - 96) * 17576) + ((n.charAt(2) - 96) * 676) +
-                    ((n.charAt(3) - 96) * 26);
-            return code;
-        }
-        else{                                                                                                           //for names that are atleast 5 letters long.-
-            code = ((n.charAt(0) - 96) * 456976) + ((n.charAt(1) - 96) * 17576) + ((n.charAt(2) - 96) * 676) +
-                    ((n.charAt(3) - 96) * 26) + (n.charAt(4) - 96);
-            return code;
-        }
-    }
 }
